5e724f0ecb187b968aed4161dbd5a2e7553a9173
Bree2003 <breeale2003@gmail.com>
2025-06-29 22:43:15 -0400

    first commit

---
+++ src/test/java/com/healthtrack/AppTest.java
@@ -0,0 +1,12 @@
+package com.healthtrack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/healthtrack/UsuarioTest.java
@@ -0,0 +1,22 @@
+package com.healthtrack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UsuarioTest {
+
+    @Test
+    public void testactualizarPeso() {
+        Usuario user = new Usuario("Ana", 70.0);
+        user.actualizarPeso(68.5);
+        assertEquals(68.5, user.getPeso(), 0.01);
+    }
+
+    @Test
+    public void testCorrectoActualizarPeso() {
+        Usuario user = new Usuario("Pedro", 78.0);
+        user.correctoActualizarPeso(70.0);
+        assertEquals(70.0, user.getPeso(), 0.01);
+    }
+}
