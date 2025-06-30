2ff543f63962c7ecfec86520fc3bbb2d7d85689b
Bree2003 <breeale2003@gmail.com>
2025-06-29 22:46:12 -0400

    update test usuario

--- src/test/java/com/healthtrack/UsuarioTest.java
+++ src/test/java/com/healthtrack/UsuarioTest.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UsuarioTest {
 
-    @Test
-    public void testactualizarPeso() {
-        Usuario user = new Usuario("Ana", 70.0);
-        user.actualizarPeso(68.5);
-        assertEquals(68.5, user.getPeso(), 0.01);
-    }
+//    @Test
+//    public void testactualizarPeso() {
+//        Usuario user = new Usuario("Ana", 70.0);
+//        user.actualizarPeso(68.5);
+//        assertEquals(68.5, user.getPeso(), 0.01);
+//    }
 
     @Test
     public void testCorrectoActualizarPeso() {
